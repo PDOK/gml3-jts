ed2c1e9fb1db6a2a2270b48845d774b477e9b334
Jurgen te Stroet <jurgen.testroet@kadaster.nl>
2015-08-18 17:11:05 +0200

    PDOK-1640 draft convertor Polygon --> GML3

--- src/main/java/nl/pdok/gml3/GML3Encoder.java
+++ src/main/java/nl/pdok/gml3/GML3Encoder.java
@@ -1,10 +1,30 @@
 package nl.pdok.gml3;
 
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectStreamField;
+import java.net.MulticastSocket;
+
 import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 
 import org.opengis.gml_3_1_1.AbstractGeometryType;
+import org.opengis.gml_3_1_1.AbstractRingPropertyType;
+import org.opengis.gml_3_1_1.AbstractRingType;
+import org.opengis.gml_3_1_1.DirectPositionListType;
+import org.opengis.gml_3_1_1.LinearRingPropertyType;
+import org.opengis.gml_3_1_1.LinearRingType;
+import org.opengis.gml_3_1_1.MultiSurfaceType;
+import org.opengis.gml_3_1_1.ObjectFactory;
+import org.opengis.gml_3_1_1.PolygonPropertyType;
+import org.opengis.gml_3_1_1.PolygonType;
+import org.opengis.gml_3_1_1.RingPropertyType;
+import org.opengis.gml_3_1_1.SurfaceArrayPropertyType;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.Polygon;
 
 public class GML3Encoder {
 	
@@ -21,4 +41,48 @@ public class GML3Encoder {
 		}
 	}
 
+	
+	public String toGML(Geometry geometry) throws JAXBException{
+		
+		
+		ObjectFactory objectFactory = new ObjectFactory();
+		
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		
+		Polygon polygon = (Polygon) geometry;
+		
+		DirectPositionListType positionList = objectFactory.createDirectPositionListType();
+		
+		Coordinate[] coordinates = polygon.getExteriorRing().getCoordinates();
+		for (Coordinate coord:coordinates){
+			positionList.getValue().add(String.valueOf(coord.x));
+			positionList.getValue().add(String.valueOf(coord.y));
+		}
+		
+		LinearRingType linearRingType = objectFactory.createLinearRingType();
+		linearRingType.setPosList(positionList);
+		
+		JAXBElement<LinearRingType> ringType = objectFactory.createLinearRing(linearRingType);
+				
+		AbstractRingPropertyType type = objectFactory.createAbstractRingPropertyType();
+		type.setRing(ringType);
+				
+		JAXBElement<AbstractRingPropertyType> ringPropertyType = objectFactory.createExterior(type);
+
+				
+		PolygonType polygonType = objectFactory.createPolygonType();
+		polygonType.setExterior(ringPropertyType);
+		
+		JAXBElement<PolygonType> polygonJaxb = objectFactory.createPolygon(polygonType);
+		
+        SurfaceArrayPropertyType surfaceArrayPropertyType = objectFactory.createSurfaceArrayPropertyType();
+		surfaceArrayPropertyType.getSurface().add(polygonJaxb);
+		
+		MultiSurfaceType multiSurfaceType = objectFactory.createMultiSurfaceType();
+		multiSurfaceType.setSurfaceMembers(surfaceArrayPropertyType);
+		
+		marshaller.marshal(objectFactory.createMultiSurface(multiSurfaceType), baos);
+		return "gml --> " + baos.toString();
+	}
+	
 }
--- src/test/java/nl/pdok/gml3/GML3EncoderTest.java
+++ src/test/java/nl/pdok/gml3/GML3EncoderTest.java
@@ -2,15 +2,28 @@ package nl.pdok.gml3;
 
 import static org.junit.Assert.*;
 
+import javax.xml.bind.JAXBException;
+
 import org.junit.Test;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.CoordinateSequenceFactory;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.Polygon;
 
 public class GML3EncoderTest {
 
 	@Test
-	public void test() {
-		// fail("Not yet implemented");
-		
+	public void test() throws JAXBException {
 		GML3Encoder encoder = new GML3Encoder();
+		
+		GeometryFactory geometryFactory = new GeometryFactory();
+		
+		Coordinate[] square = { new Coordinate(1, 1), new Coordinate(1, 5), new Coordinate(5, 5), new Coordinate(5, 1), new Coordinate(1, 1)};
+		
+		Polygon polygon_1 = geometryFactory.createPolygon(square);
+		
+		System.out.println(encoder.toGML(polygon_1));
 		
 	}
 
